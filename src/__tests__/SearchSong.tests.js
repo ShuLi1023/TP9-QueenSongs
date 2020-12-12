2d5730a54700e9350172b375bf0b1667fcb29b58
R-Aniruddha <aniruddha04@gmail.com>
2020-12-12 18:39:56 +0100

    Fix Warnings

--- src/__tests__/SearchSong.tests.js
+++ src/__tests__/SearchSong.tests.js
@@ -9,6 +9,7 @@ import {
 import SearchSong from "../Components/SearchSong"
 import axiosMock from "axios"
 
+beforeEach(jest.spyOn(window, "alert").mockImplementation(() => {}))
 afterEach(cleanup)
 
 test("Check text rendered correctly", () => {
@@ -22,23 +23,6 @@ test("Check text rendered correctly", () => {
 
 	const placeholderText = componentRendered.getByLabelText("Search Songs")
 	expect(placeholderText).toBeInTheDocument()
-})
-
-test("Check noOptionsText for invalid Song Name", () => {
-	const componentRendered = render(
-		<SearchSong
-			selectedSongs={[]}
-			onSelectSong={jest.fn()}
-			onRemoveSong={jest.fn()}
-		/>
-	)
-
-	const autocomplete = componentRendered.getByLabelText("Search Songs")
-
-	fireEvent.change(autocomplete, { target: { value: "12345" } })
-
-	const element = componentRendered.getByText("No Song With That Name")
-	expect(element).toBeInTheDocument()
 })
 
 test("Check correct Song Name generated for given input", async () => {
@@ -56,7 +40,6 @@ test("Check correct Song Name generated for given input", async () => {
 	)
 
 	const autocomplete = getByLabelText("Search Songs")
-	expect(autocomplete).toBeInTheDocument()
 
 	act(() => {
 		fireEvent.change(autocomplete, { target: { value: "dozen" } })
@@ -86,7 +69,6 @@ test("Check 'No Song With Name' msg for invalid song name", async () => {
 	)
 
 	const autocomplete = getByLabelText("Search Songs")
-	expect(autocomplete).toBeInTheDocument()
 
 	act(() => {
 		fireEvent.change(autocomplete, { target: { value: "12345" } })
@@ -97,3 +79,32 @@ test("Check 'No Song With Name' msg for invalid song name", async () => {
 	expect(axiosMock.get).toHaveBeenCalledTimes(1)
 	expect(axiosMock.get).toHaveBeenCalledWith("http://localhost:8081/12345")
 })
+/*
+test("API not running", async () => {
+	axiosMock.get.mockResolvedValueOnce({
+		data: [],
+		status: 400,
+	})
+	global.alert = jest.fn()
+
+	const { getByLabelText } = render(
+		<SearchSong
+			selectedSongs={[]}
+			onSelectSong={jest.fn()}
+			onRemoveSong={jest.fn()}
+		/>
+	)
+
+	const autocomplete = getByLabelText("Search Songs")
+
+	act(() => {
+		fireEvent.change(autocomplete, { target: { value: "dozen" } })
+	})
+
+	expect(global.alert).toHaveBeenCalledTimes(1)
+
+	expect(axiosMock.get).toHaveBeenCalledTimes(1)
+	expect(axiosMock.get).toHaveBeenCalledWith("http://localhost:8081/dozen")
+	//expect().toHaveBeenCalledTimes(1)
+})
+*/
