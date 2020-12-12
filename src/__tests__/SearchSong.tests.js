271fec3fea07a52848822221c25d6ff11443714d
lishu <shuli.sue1023@gmail.com>
2020-12-12 19:21:28 +0100

    add select and unselect test

--- src/__tests__/SearchSong.tests.js
+++ src/__tests__/SearchSong.tests.js
@@ -79,6 +79,58 @@ test("Check 'No Song With Name' msg for invalid song name", async () => {
 	expect(axiosMock.get).toHaveBeenCalledTimes(1)
 	expect(axiosMock.get).toHaveBeenCalledWith("http://localhost:8081/12345")
 })
+
+test("Check select a song", async () => {
+	axiosMock.get.mockResolvedValueOnce({
+		data: ["A Dozen Red Roses For My Darling"],
+		status: 200,
+	})
+	const mockSelectSong = jest.fn()
+	const { getByLabelText, getByText } = render(
+		<SearchSong
+			selectedSongs={[]}
+			onSelectSong={mockSelectSong}
+			onRemoveSong={jest.fn()}
+		/>
+	)
+
+	const autocomplete = getByLabelText("Search Songs")
+
+	act(() => {
+		fireEvent.change(autocomplete, { target: { value: "dozen" } })
+	})
+	const selectedSong = await waitFor(() => getByText("A Dozen Red Roses For My Darling"))
+	fireEvent.click(selectedSong)
+
+	expect(mockSelectSong).toHaveBeenCalledTimes(1)
+	expect(mockSelectSong).toHaveBeenCalledWith("A Dozen Red Roses For My Darling")
+})
+
+test("Check unselect a song", async () => {
+	axiosMock.get.mockResolvedValueOnce({
+		data: ["A Dozen Red Roses For My Darling"],
+		status: 200,
+	})
+	const mockUnSelectSong = jest.fn()
+	const { getByLabelText, getByText } = render(
+		<SearchSong
+			selectedSongs={["A Dozen Red Roses For My Darling"]}
+			onSelectSong={jest.fn()}
+			onRemoveSong={mockUnSelectSong}
+		/>
+	)
+
+	const autocomplete = getByLabelText("Search Songs")
+
+	act(() => {
+		fireEvent.change(autocomplete, { target: { value: "dozen" } })
+	})
+	const selectedSong = await waitFor(() => getByText("A Dozen Red Roses For My Darling"))
+	fireEvent.click(selectedSong)
+
+	expect(mockUnSelectSong).toHaveBeenCalledTimes(1)
+	expect(mockUnSelectSong).toHaveBeenCalledWith("A Dozen Red Roses For My Darling")
+})
 /*
 test("API not running", async () => {
 	axiosMock.get.mockResolvedValueOnce({
